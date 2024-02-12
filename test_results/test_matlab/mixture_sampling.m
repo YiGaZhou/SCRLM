69d63c67029236ca5232bfa8fe32f4a4a50c165a
Yijia Zhou <yz17d@my.fsu.edu>
2024-02-12 17:43:18 +0800

    delete

--- test_results/test_matlab/mixture_sampling.m
+++
@@ -1,6 +0,0 @@
-function probabilities = mixture_sampling(points, center_indices, alpha, distances_squared)
-    m = length(points);
-    total_distances_squared = sum(distances_squared);
-    
-    probabilities = ((1 - alpha) * distances_squared / total_distances_squared) + (alpha / m);
-end
